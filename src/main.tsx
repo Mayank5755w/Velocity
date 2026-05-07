d19b1e3c04d71e219dc81a9502542a172c132c7b
Mayank <mayank5755w@gmail.com>
2026-05-07 10:21:32 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { Analytics } from "@vercel/analytics/react"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
